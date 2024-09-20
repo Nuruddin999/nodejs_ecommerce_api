45a9e5d6abb1fab4403120bf7746aa9e0e80ffa0
nuruddin <sg7720@gmail.com>
2024-09-20 12:08:34 +0300

    run allready created initadmin function

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const compression = require("compression");
 const unknownEndpoint = require("./middleware/unKnownEndpoint");
 const { handleError } = require("./helpers/error");
 const path = require("path");
-const pool = require("./config");
-const { hashPassword } = require("./helpers/hashPassword");
+const { initAdmin } = require("./controllers/auth.controller");
 
 const app = express();
 
@@ -32,17 +31,8 @@ app.get("/", (req, res) =>
 app.use(unknownEndpoint);
 app.use(handleError);
 
+initAdmin()
 
-const createUserDbAdmin = async ({ username, password, email, fullname, roles }) => {
-  const { rows: user } = await pool.query(
-    `INSERT INTO users(username, password, email, fullname, roles) 
-    VALUES($1, $2, $3, $4, $5) 
-    returning user_id, username, email, fullname, roles, address, city, state, country, created_at`,
-    [username, password, email, fullname, roles]
-  );
-};
-const hashedPassword = hashPassword('Fillsilvershopp999@');
-createUserDbAdmin({username:"Silvershop",password:hashedPassword,email:'sg7720@gmail.com',fullname:'NGadzhiev',roles:['admin']})
 
 
 
