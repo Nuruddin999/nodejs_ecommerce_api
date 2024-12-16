d466fb362ffe539ddd8d022be6f1776a10c5dad3
nuruddin <sg7720@gmail.com>
2024-12-16 20:18:44 +0300

    change path to static

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
 }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/images/")));
 app.use(session(sessionConfig));
 
 app.use(passport.initialize());
